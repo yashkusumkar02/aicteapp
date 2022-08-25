2d76df6b04ccc1dceaba860a1aacb2fe863ff712
yashkusumkar02 <kusumkarsuyash1234@gmail.com>
2022-08-25 10:03:34 +0530

    AICTE APP

--- app/src/main/java/com/example/aicte/About_us.java
+++ app/src/main/java/com/example/aicte/About_us.java
@@ -5,10 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toolbar;
 
 public class About_us extends AppCompatActivity {
-    Toolbar toolbar;
+    Button back_btn,home,home_btn;
 
 
     @Override
@@ -16,9 +17,18 @@ public class About_us extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_us);
 
-        toolbar=findViewById(R.id.toolbar);
+        back_btn=findViewById(R.id.back_btn);
+        home_btn=findViewById(R.id.home_btn_press);
 
-        toolbar.setOnClickListener(new View.OnClickListener() {
+        back_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(About_us.this,MainActivity.class));
+                finish();
+            }
+        });
+
+        home_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(About_us.this,MainActivity.class));
